79ddf89f6feb4c985f6c09704da82b4926dc0c74
Catalin Pit <catalinpit@gmail.com>
2019-06-28 10:11:37 +0300

    added EJS

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 const express = require('express')
 // use Express
 const app = express();
+// set the application to use Embedded Javascript - render HTML instead of plain text
+app.set('view engine', 'ejs');
 
 // route for the root url
-// when accessing the "/" route, the web app will render 
+// when accessing the "/" route, the web app will render the homepage
 app.get('/', (req, res) => {
-    return res.send('Hello World!')
+    return res.render('homepage')
 })
 
-// configure the server
+// configure the server to run on port 3000
 app.listen(3000, () => {
     console.log("Palindrome database up and running, on port 3000")
 })
