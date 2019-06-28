09519fcdeceb910ab63e218867c9885c915fdd55
Catalin Pit <catalinpit@gmail.com>
2019-06-28 12:06:49 +0300

    added the palindrome checker function

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set('view engine', 'ejs');
 // use bodyParser middleware - allows us to manipulate the key-value pairs stored in the request object (req.body)
 app.use(bodyParser.urlencoded({extended: true}))
 
+// function that checks whether a String is a palindrome or not
+function isPalindrome(userStr) { 
+    // removes the special chars and spaces and transforms it to lowercase
+    // g - global matching (all matches)
+    // i - insensitive match - ignore letter case (case insensitive)
+    newStr = userStr.replace(/[^A-Z0-9]/gi, "").toLowerCase()
+    return newStr === newStr.split('').reverse().join('');
+}
+
 var pals = ["Dammit I'm Mad", "Race car"]
 // transform the array into lowercase so it can be compared later with the palindrome entered by the user
 var palindromes = pals.map((palindrome) => { return palindrome.toLowerCase() })
@@ -27,9 +36,13 @@ app.post('/addpalindrome', (req, res) => {
     var isInArray = palindromes.includes(newpalindrome);
 
     if (isInArray) {
-        console.log('Error')
+        console.log('Error - palindrome already exists in the db')
     } else {
-        palindromes.push(newpalindrome)
+        if (isPalindrome(newpalindrome)) {
+            palindromes.push(newpalindrome)
+        } else {
+            console.log('Error - the string entered is not a palindrome')
+        }
     }
 
     res.redirect('/');
